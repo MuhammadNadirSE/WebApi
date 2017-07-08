5837681832a8d0dab69a570d11335ace50854f22
Muhammad Nadir <muhammad.nadir@afiniti.com>
2017-07-09 00:36:19 +0500

    first commit of this repository

---
+++ APIs/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIs.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+        public string Get(int id, string name)
+        {
+            return "value";
+        }
+        // GET: api/Products/phone
+        [HttpGet]
+        //[Route("api/Products/myfunc")]
+        public string Custom(string id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        public void Post()
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
